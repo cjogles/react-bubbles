93258a6cf5076bdb241c7b7e26e75babc5c5fcf5
cjogles <jackson.ogles28@gmail.com>
2019-11-15 09:27:09 -0700

    Structure set up for get request and authentication and private route and login page

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Redirect } from "react-router-dom";
+
+const isAuthenticated = () => {
+  return localStorage.getItem("token") ? true : false;
+};
+export function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated() ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
